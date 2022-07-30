18df3e20abc38726ec46f5a3c22fe58bc8644d76
taohui <htao@thoughtworks.com>
2022-07-30 21:44:39 +0800

    test checkbox can disable button

--- src/App.js
+++ src/App.js
@@ -4,14 +4,18 @@ import './App.css';
 
 function App() {
   const [buttonColor, setButtonColor] = useState("red")
+  const [disabled, setDisabled] = useState(false)
   const newButtonColor = buttonColor === "red" ? "blue" : "red"
   return (
     <div>
       <button 
       style={{backgroundColor: buttonColor}}
+      disabled={disabled}
       onClick={()=>setButtonColor(newButtonColor)}
       >
-        change to {newButtonColor}</button>
+        change to {newButtonColor}
+        </button>
+        <input type="checkbox" onChange={(e)=> setDisabled(e.target.checked)}/>
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -8,12 +8,44 @@ test('button has initial color', () => {
 });
 
 test('button turns to blue when click it', ()=> {
-
   render(<App />)
   const button = screen.getByRole("button", {name: "change to blue"})
-
   fireEvent.click(button)
   expect(button).toHaveStyle({backgroundColor: "blue"})
   expect(button.textContent).toBe("change to red")
-
 });
+
+test('button and checkbox initial conditions', () => {
+  render(<App />)
+
+  const button = screen.getByRole("button", {name: "change to blue"})
+  expect(button).toBeEnabled();
+
+  const checkbox = screen.getByRole("checkbox")
+  expect(checkbox).not.toBeChecked()
+})
+
+test("when checkbox checked, button should be disabled", ()=>{
+
+  render(<App />)
+  const checkbox = screen.getByRole("checkbox");
+  fireEvent.click(checkbox);
+
+  const button = screen.getByRole("button", {name: "change to blue"})
+  expect(button).toBeDisabled();
+})
+
+test("when checkbox unchecked, button should be enabled", ()=>{
+  render(<App />)
+
+  const button = screen.getByRole("button", {name: "change to blue"})
+  expect(button).toBeEnabled();
+
+  const checkbox = screen.getByRole("checkbox");
+  fireEvent.click(checkbox)
+
+  expect(button).toBeDisabled()
+
+  fireEvent.click(checkbox)
+  expect(button).toBeEnabled()
+})
