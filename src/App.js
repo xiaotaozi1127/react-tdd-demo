78168593403936423152182eb7465395a229595e
taohui <htao@thoughtworks.com>
2022-07-31 20:25:04 +0800

    use prettier format code

--- src/App.js
+++ src/App.js
@@ -1,28 +1,31 @@
-import { useState } from 'react';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 export function replaceCapitalCamelcaseColorName(colorName) {
   return colorName.replace(/\B([A-Z])\B/g, " $1");
 }
 
 function App() {
-  const [buttonColor, setButtonColor] = useState("MediumVioletRed")
-  const [disabled, setDisabled] = useState(false)
-  const newButtonColor = buttonColor === "MediumVioletRed" ? "MidnightBlue": "MediumVioletRed"
+  const [buttonColor, setButtonColor] = useState("MediumVioletRed");
+  const [disabled, setDisabled] = useState(false);
+  const newButtonColor =
+    buttonColor === "MediumVioletRed" ? "MidnightBlue" : "MediumVioletRed";
   return (
     <div>
-      <button 
-      style={{backgroundColor: disabled ? "gray" : buttonColor}}
-      disabled={disabled}
-      onClick={()=>setButtonColor(newButtonColor)}
+      <button
+        style={{ backgroundColor: disabled ? "gray" : buttonColor }}
+        disabled={disabled}
+        onClick={() => setButtonColor(newButtonColor)}
       >
         Change to {replaceCapitalCamelcaseColorName(newButtonColor)}
-        </button>
-        <input type="checkbox"
-        id = "disable-button-checkbox"
+      </button>
+      <input
+        type="checkbox"
+        id="disable-button-checkbox"
         defaultChecked={disabled}
-         onChange={(e)=> setDisabled(e.target.checked)}/>
-         <label htmlFor='disable-button-checkbox'>Disable button</label>
+        onChange={(e) => setDisabled(e.target.checked)}
+      />
+      <label htmlFor="disable-button-checkbox">Disable button</label>
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,82 +1,97 @@
-import { render, screen, fireEvent } from '@testing-library/react';
-import App from './App';
-import {replaceCapitalCamelcaseColorName} from './App';
+import { render, screen, fireEvent } from "@testing-library/react";
+import App from "./App";
+import { replaceCapitalCamelcaseColorName } from "./App";
 
-test('button has initial color', () => {
+test("button has initial color", () => {
   render(<App />);
-  const button = screen.getByRole("button", {name: "Change to Midnight Blue"});
-  expect(button).toHaveStyle('background-color:MediumVioletRed');
+  const button = screen.getByRole("button", {
+    name: "Change to Midnight Blue",
+  });
+  expect(button).toHaveStyle("background-color:MediumVioletRed");
 });
 
-test('button turns to blue when click it', ()=> {
-  render(<App />)
-  const button = screen.getByRole("button", {name: "Change to Midnight Blue"})
-  fireEvent.click(button)
-  expect(button).toHaveStyle('background-color:MidnightBlue')
-  expect(button).toHaveTextContent("Change to Medium Violet Red")
+test("button turns to blue when click it", () => {
+  render(<App />);
+  const button = screen.getByRole("button", {
+    name: "Change to Midnight Blue",
+  });
+  fireEvent.click(button);
+  expect(button).toHaveStyle("background-color:MidnightBlue");
+  expect(button).toHaveTextContent("Change to Medium Violet Red");
 });
 
-test('button and checkbox initial conditions', () => {
-  render(<App />)
+test("button and checkbox initial conditions", () => {
+  render(<App />);
 
-  const button = screen.getByRole("button", {name: "Change to Midnight Blue"})
+  const button = screen.getByRole("button", {
+    name: "Change to Midnight Blue",
+  });
   expect(button).toBeEnabled();
 
-  const checkbox = screen.getByRole("checkbox")
-  expect(checkbox).not.toBeChecked()
-})
+  const checkbox = screen.getByRole("checkbox");
+  expect(checkbox).not.toBeChecked();
+});
 
-test("when checkbox checked, button should be disabled", ()=>{
-
-  render(<App />)
+test("when checkbox checked, button should be disabled", () => {
+  render(<App />);
   const checkbox = screen.getByRole("checkbox");
   fireEvent.click(checkbox);
 
-  const button = screen.getByRole("button", {name: "Change to Midnight Blue"})
+  const button = screen.getByRole("button", {
+    name: "Change to Midnight Blue",
+  });
   expect(button).toBeDisabled();
-})
+});
 
-test("when checkbox unchecked, button should be enabled", ()=>{
-  render(<App />)
+test("when checkbox unchecked, button should be enabled", () => {
+  render(<App />);
 
-  const button = screen.getByRole("button", {name: "Change to Midnight Blue"})
+  const button = screen.getByRole("button", {
+    name: "Change to Midnight Blue",
+  });
   expect(button).toBeEnabled();
 
-  const checkbox = screen.getByRole("checkbox", {name: "Disable button"});
-  fireEvent.click(checkbox)
+  const checkbox = screen.getByRole("checkbox", { name: "Disable button" });
+  fireEvent.click(checkbox);
 
-  expect(button).toBeDisabled()
+  expect(button).toBeDisabled();
 
-  fireEvent.click(checkbox)
-  expect(button).toBeEnabled()
-})
+  fireEvent.click(checkbox);
+  expect(button).toBeEnabled();
+});
 
-test("button turns gray when disabled", ()=>{
-  render(<App />)
+test("button turns gray when disabled", () => {
+  render(<App />);
 
-  const button = screen.getByRole("button", {name: "Change to Midnight Blue"})
-  const checkbox = screen.getByRole("checkbox", {name: "Disable button"})
-  fireEvent.click(checkbox)
+  const button = screen.getByRole("button", {
+    name: "Change to Midnight Blue",
+  });
+  const checkbox = screen.getByRole("checkbox", { name: "Disable button" });
+  fireEvent.click(checkbox);
 
-  expect(button).toHaveStyle('background-color:gray')
+  expect(button).toHaveStyle("background-color:gray");
 
-  fireEvent.click(checkbox)
-  expect(button).toHaveStyle('background-color:MediumVioletRed')
+  fireEvent.click(checkbox);
+  expect(button).toHaveStyle("background-color:MediumVioletRed");
 
-  fireEvent.click(checkbox)
-  expect(button).toHaveStyle('background-color:gray')
-})
+  fireEvent.click(checkbox);
+  expect(button).toHaveStyle("background-color:gray");
+});
 
-describe("space before camel-case capital letters", ()=> {
-  test("works for single word", ()=>{
+describe("space before camel-case capital letters", () => {
+  test("works for single word", () => {
     expect(replaceCapitalCamelcaseColorName("Red")).toBe("Red");
-  })
+  });
 
-  test("works for two camelcase words", ()=>{
-    expect(replaceCapitalCamelcaseColorName("MidnightBlue")).toBe("Midnight Blue");
-  })
+  test("works for two camelcase words", () => {
+    expect(replaceCapitalCamelcaseColorName("MidnightBlue")).toBe(
+      "Midnight Blue"
+    );
+  });
 
-  test("works for multiple camel case words", ()=>{
-    expect(replaceCapitalCamelcaseColorName("MediumVioletRed")).toBe("Medium Violet Red");
-  })
-})
+  test("works for multiple camel case words", () => {
+    expect(replaceCapitalCamelcaseColorName("MediumVioletRed")).toBe(
+      "Medium Violet Red"
+    );
+  });
+});
